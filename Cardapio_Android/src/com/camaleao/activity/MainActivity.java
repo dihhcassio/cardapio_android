e6c667a774819daf3b9d368f0be6b469b45b6c7d
dihhcassio <adricarr@gmail.com>
2014-11-05 14:17:28 -0200

    [+] - Commit inicial

---
+++ Cardapio_Android/src/com/camaleao/activity/MainActivity.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.camaleao.activity;
+
+import com.camaleao.cardapio.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Adriano
+ *
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
+
+}
