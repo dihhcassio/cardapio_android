016708bd96c0bdd21455ca4f8696a1c96accd732
Adriano <adricarr@gamil.com>
2014-11-17 11:56:23 -0300

    [+] Incio da pesquisa

--- Cardapio_Android/src/com/camaleao/cardapio/Activity/LoginActivity.java
+++ Cardapio_Android/src/com/camaleao/cardapio/Activity/LoginActivity.java
@@ -1,19 +1,51 @@
 package com.camaleao.cardapio.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.camaleao.cardapio.R;
+import com.camaleao.cardapio.Conector.CardapioConector;
 
 /**
  * Created by adriano on 02/11/14.
  */
 public class LoginActivity extends Activity {
+	
+	private TextView user;
+	private TextView pass;
+	private Button logar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 
+		this.user = (TextView) findViewById(R.id.login_user);
+		this.pass = (TextView) findViewById(R.id.login_pass);
+		this.logar = (Button) findViewById(R.id.login_logar);
+		
+		this.logar.setOnClickListener(getListenerLogar());
+	}
+
+	private OnClickListener getListenerLogar() {
+		return new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (CardapioConector.buildConector().logar(LoginActivity.this.user.getText().toString(), 
+						LoginActivity.this.pass.getText().toString())){
+					Intent it = new Intent(LoginActivity.this, MainActivity.class);
+					LoginActivity.this.startActivity(it);
+				} else
+					Toast.makeText(LoginActivity.this, "Não foi possível logar", Toast.LENGTH_LONG).show();				
+			}
+		};
 	}
 
 }
--- Cardapio_Android/src/com/camaleao/cardapio/Activity/MainActivity.java
+++ Cardapio_Android/src/com/camaleao/cardapio/Activity/MainActivity.java
@@ -7,6 +7,8 @@ import com.camaleao.cardapio.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * @author Adriano
@@ -14,10 +16,20 @@ import android.os.Bundle;
  */
 public class MainActivity extends Activity {
 
+	
+	private Button btPesquisar;
+	private TextView txtPesquisar;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		
+		this.btPesquisar = (Button) findViewById(R.id.main_pesquisar);
+		this.txtPesquisar = (TextView) findViewById(R.id.main_text);
+		
+		this.btPesquisar.setOnClickListener(getPesquisarListene);
 	}
+
 
 }
